a856d49118767a7df162dc6584a42f2c9715d4ea
Victor Ihuoma <vicky.ieva5115@gmail.com>
2024-02-13 13:26:45 +0100

    Initial commit

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = os.getenv('FLASK_KEY')
+app.config['SECRET_KEY'] = os.getenv('FLASK_KEY')
 # ckeditor = CKEditor(app)
 # Bootstrap5(app)
 #
